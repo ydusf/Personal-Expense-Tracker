4347496179739281a6d66d4b2b732554c2e34cbc
sebastianlatham <LathamS2@cardiff.ac.uk>
2024-09-21 18:33:47 +0100

    db setup fix

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -124,15 +124,11 @@ WSGI_APPLICATION = "expense_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 CATEGORIES = [
